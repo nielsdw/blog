5b9fe102daeb820ee9931f8f85522e23f6f84a97
Niels de Water <nielsdw@gmail.com>
2021-07-19 15:50:25 +0200

    remove unused import

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
 from blog.serializers import BlogSerializer, BlogListSerializer, BlogPatchSerializer
 from blog.models import Blog
 from django.template.defaultfilters import slugify
